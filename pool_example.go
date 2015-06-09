14701ad20f9c6d2bbc31843d6674e89b40692575
duwei <pandorid@gmail.com>
2015-06-09 19:10:28 +0800

    add example

---
+++ pool_example.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"net"
+	"time"
+	"fmt"
+	"errors"
+	"github.com/wesdu/goutil/pool"
+)
+
+func tcp_testserver() {
+	ln, err := net.Listen("tcp", ":9999")
+	if err != nil {
+		// handle error
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			// handle error
+		}
+		go func() {
+			fmt.Println("server got:", conn)
+			fmt.Fprintf(conn, "hi\r\n")
+		}()
+	}
+}
+
+func newPool() *pool.Pool {
+	return &pool.Pool{
+		IdleTimeout: 15 * time.Minute,
+		Dial: func() (pool.Conn, error) {
+			return pool.Dial("tcp", ":9999")
+		},
+		TestOnBorrow: func(c pool.Conn, t time.Time) error {
+			return errors.New("test")
+		},
+		Wait: true,
+	}
+}
+
+func main() {
+	done := make(chan bool)
+
+	go tcp_testserver()
+	time.Sleep(1 * time.Second)
+
+	pool := newPool()
+	if c, err := pool.Get(); err == nil {
+		c.WriteStringLine("hello world")
+		bb, err := c.ReadBytesLine()
+		if err == nil {
+			fmt.Println(string(bb))
+		}
+		c.Close()
+		fmt.Println(c)
+	} else {
+		fmt.Println(err)
+	}
+	if c, err := pool.Get(); err == nil {
+		c.WriteStringLine("hello world2")
+		bb, err := c.ReadBytesLine()
+		if err == nil {
+			fmt.Println(string(bb))
+		}
+		c.Close()
+	}else{
+		fmt.Println(err)
+	}
+	fmt.Println(pool)
+
+	<-done
+}
+
