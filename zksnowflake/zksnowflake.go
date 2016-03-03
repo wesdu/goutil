32567d646142c615b776053f0c2e1a0eee07ecc9
duwei <pandorid@gmail.com>
2016-03-04 02:46:09 +0800

    zksnowflake done

--- zksnowflake/zksnowflake.go
+++ zksnowflake/zksnowflake.go
@@ -1,7 +1,12 @@
-package main
+package zksnowflake
 
 import (
-	"fmt"
+	"github.com/samuel/go-zookeeper/zk"
+	"log"
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
 	"time"
 )
 
@@ -14,6 +19,24 @@ const (
 	max_process_id   = 1 << process_id_bits
 	max_sequence_id  = 1 << sequence_id_bits
 )
+
+const (
+	LOCK_PATH = "/_snowflake_/lock/"
+	NODE_PATH = "/_snowflake_/nodes/"
+)
+
+var (
+	_zk_hosts   string
+	_zk_conn    *zk.Conn
+	_sfc_map    map[string]*SnowFlakeCloud
+	global_lock sync.RWMutex
+)
+
+type int64arr []int64
+
+func (a int64arr) Len() int           { return len(a) }
+func (a int64arr) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a int64arr) Less(i, j int) bool { return a[i] < a[j] }
 
 func make_timestamp() int64 {
 	return time.Now().UnixNano() / int64(time.Millisecond)
@@ -46,15 +69,171 @@ func snowflake_expire_date() time.Time {
 	return time.Unix((max_timestamp-1+twepoch)/1000, 0)
 }
 
-func dialog() {
+func Dialog() {
 	t := make_timestamp()
 	sid := make_snowflake(t, 1, 2)
-	fmt.Println(melt_snowflakeid(sid))
-	fmt.Println("max id", snowflake_maxid())
-	fmt.Println("algorithm expire at", snowflake_expire_date())
-	fmt.Println("snowflake id:", sid)
+	log.Println(melt_snowflakeid(sid))
+	log.Println("max id", snowflake_maxid())
+	log.Println("max process id", max_process_id)
+	log.Println("algorithm expire at", snowflake_expire_date())
+	log.Println("snowflake id:", sid)
 
 }
-func main() {
-	dialog()
+
+type SnowFlake struct {
+	sequence   int64
+	last_ts    int64
+	last_seq   int64
+	process_id int64
+	lock       sync.RWMutex
+}
+
+func (sf *SnowFlake) getSequnce() int64 {
+	sf.sequence += 1
+	sf.sequence %= max_sequence_id
+	return sf.sequence
+}
+
+func (sf *SnowFlake) gen() int64 {
+	sf.lock.Lock()
+	defer sf.lock.Unlock()
+	for {
+		ts := make_timestamp()
+		seq := sf.getSequnce()
+		if ts == sf.last_ts && seq <= sf.last_seq {
+			time.Sleep(1 * time.Millisecond)
+		} else {
+			sid := make_snowflake(ts, sf.process_id, seq)
+			sf.last_ts = ts
+			sf.last_seq = seq
+			return sid
+		}
+	}
+}
+
+type SnowFlakeCloud struct {
+	zk_namespace string //show be unique for each id field
+	lock_path    string
+	node_path    string
+	zk_lock      *zk.Lock
+	sf           *SnowFlake
+}
+
+func (sfc *SnowFlakeCloud) ensure_paths(paths string) {
+	_path := ""
+	for _, p := range strings.Split(paths[1:], "/") {
+		_path += "/" + p
+		_, err := _zk_conn.Create(_path, []byte{1}, 0, zk.WorldACL(zk.PermAll))
+		if err == nil || err == zk.ErrNodeExists {
+
+		} else {
+			log.Panicln(err)
+		}
+	}
+}
+
+func (sfc *SnowFlakeCloud) Init() {
+	sfc.ensure_paths(sfc.lock_path)
+	sfc.ensure_paths(sfc.node_path)
+	sfc.zk_lock = zk.NewLock(_zk_conn, sfc.lock_path, zk.WorldACL(zk.PermAll))
+}
+
+func (sfc *SnowFlakeCloud) register() {
+	err := sfc.zk_lock.Lock()
+	if err != nil {
+		log.Panicln(err)
+	}
+	nodes, _, err := _zk_conn.Children(sfc.node_path)
+	if err != nil {
+		log.Panicln(err)
+	}
+	var pid int64
+	pid = 1
+	if len(nodes) > 0 {
+		node_ids := make(int64arr, 0, len(nodes))
+		for _, s := range nodes {
+			i, _ := strconv.ParseInt(s, 10, 64)
+			node_ids = append(node_ids, i)
+		}
+		sort.Sort(node_ids)
+		log.Println(node_ids)
+		if node_ids[len(node_ids)-1] < max_process_id-1 {
+			pid = node_ids[len(node_ids)-1] + 1
+		} else {
+			log.Println(node_ids[len(node_ids)-1])
+			for i := int64(0); i < node_ids[len(node_ids)-1]+1; i++ {
+				log.Println(i)
+				if i+1 != node_ids[i] {
+					pid = i + 1
+					break
+				} else if i == int64(len(node_ids))-1 {
+					pid = i + 2
+					break
+				} else if node_ids[i] != node_ids[i+1]-1 {
+					pid = node_ids[i] + 1
+					break
+				}
+			}
+		}
+	}
+	log.Println("process_id", pid)
+	_, err = _zk_conn.Create(sfc.node_path+"/"+strconv.FormatInt(pid, 10),
+		[]byte{1}, zk.FlagEphemeral,
+		zk.WorldACL(zk.PermAll),
+	)
+	if err != nil {
+		log.Panicln(err)
+	}
+	sfc.sf = &SnowFlake{process_id: pid}
+	err = sfc.zk_lock.Unlock()
+	if err != nil {
+		log.Panicln(err)
+	}
+}
+
+func (sfc *SnowFlakeCloud) Gen() int64 {
+	return sfc.sf.gen()
+}
+
+func Setup(zk_hosts string) {
+	global_lock.Lock()
+	defer global_lock.Unlock()
+	//only execute once
+	if _zk_hosts != "" {
+		log.Println("zookeeper already setup", _zk_hosts, _zk_conn)
+		return
+	}
+	_zk_hosts = zk_hosts
+	log.Println("set zookeeper hosts:", zk_hosts)
+	z, _, err := zk.Connect(strings.Split(_zk_hosts, ","), time.Second) //*10)
+	if err != nil {
+		log.Panicln("zookeeper fail", err)
+	} else {
+		log.Println("zookeeper connected")
+	}
+	_zk_conn = z
+}
+
+func GetGenerator(namespace string) *SnowFlakeCloud {
+	//if in the map return
+	//else create a new one
+	global_lock.Lock()
+	defer global_lock.Unlock()
+	if sfc, exist := _sfc_map[namespace]; exist {
+		return sfc
+	} else {
+		sfc = &SnowFlakeCloud{
+			zk_namespace: namespace,
+			node_path:    NODE_PATH + namespace,
+			lock_path:    LOCK_PATH + namespace,
+		}
+		sfc.Init()
+		_sfc_map[namespace] = sfc
+		sfc.register()
+		return sfc
+	}
+}
+
+func init() {
+	_sfc_map = make(map[string]*SnowFlakeCloud)
 }
---
+++ zksnowflake_example.go
@@ -0,0 +1,16 @@
+package main
+import (
+	"./zksnowflake"
+	"time"
+	"fmt"
+)
+
+func main() {
+	zksnowflake.Dialog()
+	zksnowflake.Setup("127.0.0.1")
+	g := zksnowflake.GetGenerator("test1")
+	for i:=0;i<1000000;i++ {
+		go fmt.Println(g.Gen())
+	}
+	time.Sleep(1*time.Second)
+}
