6f8efb65bf1c5ab8e98ecc6130349300a6b3d1b3
duwei <pandorid@gmail.com>
2016-03-04 02:52:04 +0800

    meme

--- zksnowflake_example.go
+++ zksnowflake_example.go
@@ -10,7 +10,7 @@ func main() {
 	zksnowflake.Setup("127.0.0.1")
 	g := zksnowflake.GetGenerator("test1")
 	for i:=0;i<1000000;i++ {
-		go fmt.Println(g.Gen())
+		go fmt.Println(g.Gen(), i)
 	}
 	time.Sleep(1*time.Second)
 }
